4d5129f112ea71740223486076c583ceb39fbda5
Sven Kayser <sk@urbanstudio.de>
2016-09-16 08:54:59 +0200

    Update Beispiel.java

--- examples/0001-Die-zwei-Objekte/dieZweiObjekte/Beispiel.java
+++ examples/0001-Die-zwei-Objekte/dieZweiObjekte/Beispiel.java
@@ -12,17 +12,17 @@ public class Beispiel {
 		
 		// Saruman ist scheisse Reich, und hat ein iPhone mit echt viel Nachrichtenspeicher
 		Besitzer saruman = new Besitzer("Saruman, der weiße",50000.0d,
-				new Handy(400.0d, 500, 1500)
+			new Handy(400.0d, 500, 1500)
 		);
 		
 		// Frodo lebt von auenländischem Hartz4, und hat nur ein Motorola
 		Besitzer frodo = new Besitzer("Frodo Beutlin", 14.0d,
-				new Handy(20.0d, 12, 200)
+			new Handy(20.0d, 12, 200)
 		);
 		
 		// Gandalf verdient anständig Geld mit Rauchschiffen, ist aber ein Hipster und hat nur ein Nokia 5110
 		Besitzer gandalf = new Besitzer("Gandalf, der graue",2000.0d,
-				new Handy(100.0d, 40,50)
+			new Handy(100.0d, 40,50)
 		);
 		
 		// Damit können wir nun einige Dinge tun.
@@ -32,7 +32,7 @@ public class Beispiel {
 		gandalf.versenden(3); // Gandalf sagt dreimal zu, weil er sich jedes mal nicht erinnern kann, ob er schon zugesagt hat.
 		frodo.versenden(1, gandalf.getHandy()); // Frodo sagt nochmal mit Gandalfs Handy zu, nur damit er sicher ist.
 		saruman.aufladen(10000.0d); // Saruman läd 10 000 € auf sein Handy weil er noch ein paar Orks einladen will
-		saruman.getHandy().speicherLöschen(); // Vorher löscht er aber nochmal seinen Speicher
+		saruman.getHandy().speicherLoeschen(); // Vorher löscht er aber nochmal seinen Speicher
 		if(!frodo.aufladen(20.0d)) // Frodo läd 20 € auf sein Handy. Wenn das aber nicht klappt (weil er mal wieder pleite ist) ....
 		{ 
 			gandalf.aufladen(200.0d, frodo.getHandy()); // .... Muss das wohl mal wieder Gandalf machen.
